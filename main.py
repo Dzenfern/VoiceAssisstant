c17d944a333ce106990e4a58d652ec7a1da76503
Dylan <dylanzfernandes@gmail.com>
2023-11-19 15:20:13 +0530

    created rudimentory base for assisstant

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
-print("hello world")
+import pyttsx3 as tts
+import speech_recognition as sr
+
+r = sr.Recognizer()
+engine = tts.init()
+
+def hear():
+    
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+    
+    try:
+        sen = r.recognize_google(audio)
+        engine.say(sen)
+        engine.runAndWait()
+        print("You said" + sen)
+    except :
+        print("Hmm...Didn't catch that")
+    
+    if sen.find('exit'):
+        return 0
+    else:
+        return 1
+n = 1
+while True:
+    val = hear()
+    if val:
+        print("\nexiting")
+        break
+
+
+
 
